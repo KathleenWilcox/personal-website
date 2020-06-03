4cbbbe448b28733c8b107dade2ac1f691367265a
KathleenWilcox <kathleenrwilcox99@gmail.com>
2020-06-02 21:49:30 -0400

    i fake fixed it

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -8,27 +8,30 @@ import Girl from '../Images/girl-with-book.jpg';
 class Post extends React.Component{
   render(){
     return(
-
-
         <div className = "post-container">
+          <div className = "post-date">
+            <div className = "undated-post-date">
+              <div className = "day">10</div>
+              <div className = "month"> Jun</div>
+              <div className = "year">2019 </div>
+            </div>
+          </div>
 
 
-
-        <div className = "post-date">
-        <div className = "undated-post-date">
-          <div className = "day">10</div>
-          <div className = "month"> Jun</div>
-          <div className = "year">2019 </div>
-        </div>
-        </div>
+          <img className = "post-img" src = {Girl} />
 
 
-        <img className = "post-img" src = {Girl} />
+          <div className= "post-header">
+            Getting Organized
+          </div>
 
-
-
-
-
+          <div className = "post-excerpt-container">
+            <p>
+              Quis eleifend quam adipiscing vitae proin sagittis nisl rh oncus. Rutrum tellus pelle ntesque eu tincidunt. Nunc mi ipsum faucibus vitae.
+              Maecenas sed enim ut sem viverra aliq uet eget sit amet. Placerat in egestas erat imperdiet sed. A pellentesque sit amet porttitor eget dolor.
+              Nibh praesent tristique magna sit amet purus gravida quis. Cursus vitaes[...]
+              </p>
+          </div>
         </div>
     );
   }
